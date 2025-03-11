432d89748cb17357c24875ace8011e9dbf853069
HaiMinh2410 <haiminh24102003@gmail.com>
2025-03-11 17:44:36 +0700

    project completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,9 +8,12 @@ import messageRoutes from './routes/message.route.js'
 import { connectDB } from './lib/db.js';
 import { app, server } from './lib/socket.js'
 
+import path from 'path'
+
 dotenv.config()
 
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: "50mb" })); // JSON tối đa 50MB
 app.use(express.urlencoded({ limit: "50mb", extended: true })); // Dữ liệu từ form
@@ -24,6 +27,14 @@ app.use(
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+  }
 
 server.listen(PORT, ()=>{
     console.log("sever is running on port: " + PORT)
